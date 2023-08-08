8c3076a5be08bdee3839d13e1e2d6fbd86169dd2
zigwaldo <mzygmunt@zigz.us>
2023-08-08 13:48:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['JavaScript', 'React', 'Vue', 'Angular', 'Node'];
+    var member = {
+        name: '张三',
+        age: 20,
+        skills: skills
+    };
+    return member;
+}
